e9ca8c15ff7935738d3d5ed9ff0e9b7d46d206aa
2560p <112290055+2560p@users.noreply.github.com>
2023-03-27 19:11:25 +0200

    Fix: merge conflicts, turns adjustments
    This version improves the turns, and the robot is now able to finish the maze.
    The version still does not have a logic for start and the end of the maze.
    The commit also resolves merge conflicts in the repo.
    
    Signed-off-by: 2560p <112290055+2560p@users.noreply.github.com>

--- CAR P-I/final/final.ino
+++ CAR P-I/final/final.ino
@@ -1,3 +1,16 @@
+/*
+    pins definition
+
+    RWF - right wheel forward
+    RWB - right wheel backward
+
+    LWF / LWB - left wheel
+
+    encoderRW / encoderLW - right / left wheel encoder
+
+    fProxEcho / fProxTrig - front proximity sensor echo / trigger
+    lProxEcho / lProxTrig - proximity sensor on the left side, echo / trigger
+*/
 
 #define RWF 6
 #define RWB 5
@@ -14,34 +27,18 @@
 #define lProxEcho 7
 #define lProxTrig 8
 
-float forwardDistance = 0;
-float leftDistance = 0;
-bool goingForward = false;
+float forwardDistance = .0f;
+float leftDistance = .0f;
 
-unsigned long countRW = 0;
-unsigned long countLW = 0;
+boolean turnedRight = false;
 
-<<<<<<< HEAD
-=======
-// boolean turnedRight = false;
-
-unsigned int countRW = 0;
-unsigned int countLW = 0;
->>>>>>> 9db7bcf (Fix: adjusted turns and timings)
+volatile int countRW = 0;
+volatile int countLW = 0;
 
 void setup()
 {
-
     Serial.begin(9600);
-<<<<<<< HEAD
-    pinMode(RWF,OUTPUT);
-    pinMode(RWB,OUTPUT);
-    pinMode(LWF,OUTPUT);
-    pinMode(LWB,OUTPUT);
-    
-=======
 
->>>>>>> 9db7bcf (Fix: adjusted turns and timings)
     pinMode(fProxEcho, INPUT);
     pinMode(fProxTrig, OUTPUT);
 
@@ -50,42 +47,27 @@ void setup()
 
     pinMode(encoderRW, INPUT);
     pinMode(encoderLW, INPUT);
+
     attachInterrupt(digitalPinToInterrupt(encoderRW), updateRW, CHANGE);
     attachInterrupt(digitalPinToInterrupt(encoderLW), updateLW, CHANGE);
-  
 }
 
-//===================================|| MAZE GO STRAIGHT FUNCTION || =======================
-void goForwardToWall()
+void loop()
 {
-<<<<<<< HEAD
-    if (leftDistance > 9.2)
-=======
     querySensors();
-    // Serial.println("Left Distance: " + String(leftDistance) + "cm, Forward distance: " + String(forwardDistance) + "cm.");
-    // return;
 
     if (leftDistance > 25)
     {
-        // stop();
-        // wait(150);
         return performLeftTurn();
     }
 
     if (leftDistance < 25 && forwardDistance < 12)
     {
-        stop();
         return performRightTurn();
     }
 
     goForward();
 }
-
-// void loop()
-// {
-//     adjustedTurnRight();
-//     return wait(1500);
-// }
 
 // movement functions
 
@@ -93,25 +75,15 @@ void goForward()
 // the function defines the behaviour of the car when it is going forward
 // it adjusts the car so that it is constantly 8.2 cm away from the wall
 {
-    if (leftDistance > 8.5)
->>>>>>> 9db7bcf (Fix: adjusted turns and timings)
+    if (leftDistance > 9.2)
     {
         analogWrite(RWF, 255);
-        // analogWrite(LWF, 232 - 10 * (leftDistance - 7));
-        analogWrite(LWF, 192);
+        analogWrite(LWF, 180);
     }
-<<<<<<< HEAD
     else if (leftDistance < 7.2)
-    {
-        analogWrite(RWF, 205);
-        // analogWrite(RWF, 255 - 10 * abs(leftDistance - 4));
-        analogWrite(LWF, 232);
-=======
-    else if (leftDistance < 5.5)
     {
         analogWrite(RWF, 225);
         analogWrite(LWF, 255);
->>>>>>> 9db7bcf (Fix: adjusted turns and timings)
     }
     else
     {
@@ -119,62 +91,133 @@ void goForward()
         analogWrite(LWF, 232);
     }
 
-    // turnedRight = false;
+    turnedRight = false;
 }
 
+void goForwardInTicks(int ticks)
+// moves the car forward for a given number of ticks
+{
+    resetCounters();
 
-<<<<<<< HEAD
-//===================================|| END OF MAZE GO STRAIGHT FUNCTION || =======================
-=======
     while (countRW < ticks)
     {
         analogWrite(RWF, 255);
         analogWrite(LWF, 232);
-
-        // Serial.println(String(countRW) + "/" + String(ticks));
-        Serial.print("");
     }
 
-    stop();
+    turnedRight = false;
 
-    // turnedRight = false;
+    stop();
+}
+
+void goBackwardInTicks(int ticks)
+{
+    resetCounters();
+
+    while (countRW < ticks)
+    {
+        analogWrite(RWB, 255);
+        analogWrite(LWB, 242);
+    }
+
+    turnedRight = false;
+
+    stop();
 }
 
 void performLeftTurn()
 {
-    // goInTicks(15);
-    goInTicks(20);
+    goForwardInTicks(20);
     wait(300);
 
     basicTurnLeft();
-    wait(300);
+
+    wait(150);
 
     querySensors();
-    if (forwardDistance > 20)
+    if (forwardDistance > 25)
     {
-        goInTicks(25);
+        goForwardInTicks(20);
     }
 
-    // turnedRight = false;
+    turnedRight = false;
+
+    return wait(150);
 }
 
 void performRightTurn()
 {
+    stop();
+
     wait(150);
 
-    basicTurnRight();
+    if (leftDistance < 6 || turnedRight)
+    {
+        basicTurnRight();
+        turnedRight = true;
+    }
+    else
+    {
+        adjustToWall();
+    }
 
-    wait(100);
+    wait(150);
 
     querySensors();
     if (forwardDistance > 15)
     {
         wait(100);
-        return goInTicks(25);
+        goForwardInTicks(25);
     }
 
-    return wait(300);
+    return wait(150);
 }
+
+// while performing a right turn, the car might need
+// to be adjusted to the wall
+void adjustToWall()
+{
+    stop();
+    resetCounters();
+
+    while (countRW < 10)
+    {
+        analogWrite(RWB, 255);
+    }
+
+    stop();
+    resetCounters();
+
+    while (countRW < 12)
+    {
+        analogWrite(RWB, 235);
+        analogWrite(LWB, 255);
+    }
+
+    stop();
+    resetCounters();
+
+    while (countRW < 12)
+    {
+        analogWrite(RWF, 255);
+    }
+
+    stop();
+    resetCounters();
+
+    while (countRW < 8)
+    {
+        analogWrite(RWF, 255);
+        analogWrite(LWF, 255);
+    }
+
+    stop();
+    resetCounters();
+}
+
+// basic movement functions
+// those are that actually do the movement
+// as an example, basicTurnLeft() turns the car left by 90 degrees
 
 void stop()
 {
@@ -184,133 +227,37 @@ void stop()
     analogWrite(LWB, 0);
 }
 
-// void turnRightLeftWheel()
-// {
-//     // Serial.println("performing a right turn with only left wheel");
-
-//     stop();
-//     resetCounters();
-
-//     while (countLW < 32)
-//     {
-//         // analogWrite(LWF, 185);
-//         analogWrite(LWF, 255);
-
-//         Serial.print("");
-//     }
-
-//     stop();
-// }
-
-// void turnRightRightWheel()
-// {
-//     // Serial.println("performing a right turn with only right wheel");
-
-//     stop();
-//     resetCounters();
-
-//     while (countRW < 32)
-//     {
-//         // analogWrite(LWF, 185);
-//         analogWrite(RWB, 255);
-
-//         Serial.print("");
-//     }
-
-//     stop();
-// }
-
-// basic movement functions
-// they are simple functions to turn precisely
-
 void basicTurnLeft()
 {
     stop();
 
     resetCounters();
-    while (countLW < 14)
+    while (countRW < 16)
     {
         analogWrite(RWF, 255);
         analogWrite(LWB, 255);
-
-        // Serial.println(String(countLW) + "/" + String(14));
-        Serial.print("");
     }
 
     stop();
 }
-
-// void basicTurnLeft()
-// {
-//     stop();
-
-//     resetCounters();
-//     while (countRW < 32)
-//     {
-//         analogWrite(RWF, 255);
-//         // analogWrite(LWB, 255);
-
-//         // Serial.println(String(countLW) + "/" + String(14));
-//         Serial.println("");
-//     }
-
-//     stop();
-// }
 
 void basicTurnRight()
 {
-    // Serial.println("performing a basic right turn");
     stop();
 
     resetCounters();
-    while (countRW < 11)
+    while (countLW < 17)
     {
         analogWrite(LWF, 255);
         analogWrite(RWB, 255);
-
-        Serial.print("");
     }
 
     stop();
 }
 
-// void adjustedTurnRight()
-// {
-//     stop();
-
-//     resetCounters();
-
-//     if (turnedRight == false)
-//     {
-//         while (countRW < 32)
-//         {
-//             // analogWrite(LWF, 185);
-//             analogWrite(RWB, 255);
-
-//             Serial.println("");
-//         }
-
-//         turnedRight = true;
-//     }
-//     else
-//     {
-//         while (countLW < 32)
-//         {
-//             // analogWrite(LWF, 185);
-//             analogWrite(LWF, 255);
-
-//             Serial.println("");
-//         }
-//     }
-
-//     stop();
-// }
-
 // counter functions
 // used to update the counters of the encoders
->>>>>>> 9db7bcf (Fix: adjusted turns and timings)
 
-//===================================|| ENCODER FUNCTIONS || ======================================
 void updateRW()
 {
     noInterrupts();
@@ -325,31 +272,24 @@ void updateLW()
     interrupts();
 }
 
-void printEncoderMesurements()
-{
-    Serial.print("Right Wheel: ");
-    Serial.print(countRW);
-    Serial.print(" , ");
-    Serial.print("Left Wheel: ");
-    Serial.println(countLW);
-}
-
 void resetCounters()
 {
     countRW = 0;
     countLW = 0;
 }
 
-//===================================|| END OF ENCODER FUNCTIONS|| ================================
+// sound sensors functions
 
-//===================================|| DISTANCE MEASUREMENT SENSORS|| ============================
 void querySensors()
+// TODO: might be optimised as the sensors always get queried together
 {
     forwardDistance = getForwardDistance();
     leftDistance = getLeftDistance();
 }
 
 float pulse(int proxTrig, int proxEcho)
+// might be renamed as it returns the distance in cm
+// TODO: also might be optimised as the sensors always get queried together
 {
     digitalWrite(proxTrig, HIGH);
     delayMicroseconds(10);
@@ -361,18 +301,25 @@ float pulse(int proxTrig, int proxEcho)
 }
 
 float getForwardDistance()
+// returns forward distance in cm
 {
     return round(pulse(fProxTrig, fProxEcho) * 100.0) / 100.0;
 }
 
 float getLeftDistance()
+// returns left distance in cm
 {
     return round(pulse(lProxTrig, lProxEcho) * 100.0) / 100.0;
 }
-//===================================|| END OF DISTANCE MEASUREMENTS || ===========================
 
-void loop()
+// other functions
+
+void wait(int timeToWait)
+// waits for an amount of time in milliseconds
+// used to eliminate the need to use the delay() function
 {
-    querySensors();
-    goForwardToWall();
+    long time = millis();
+
+    while (millis() < time + timeToWait)
+        ;
 }
