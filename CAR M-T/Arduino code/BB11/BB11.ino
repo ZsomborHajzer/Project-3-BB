3a37f64dce4c424be8af69bf563601732fcd264a
timothyadewale <adewale.adewale@student.nhlstenden.com>
2023-03-30 16:45:36 -0400

    Added some functions and more code, added comments and did some code formatting

--- CAR M-T/Arduino code/BB11/BB11.ino
+++ CAR M-T/Arduino code/BB11/BB11.ino
@@ -1,3 +1,6 @@
+
+//INCLUDED LIBRARIES
+
 #include <Adafruit_NeoPixel.h>
 #include <Servo.h>
 
@@ -27,7 +30,8 @@ const int sensorPin8 = A7; // Sensor pin 8
 
 #define distanceThreshold 15 // threshold distance in cm to detect an obstacle
 
-int endLineCounter = 0;
+
+
 // Define the pins for the left and right motor drivers
 const int rightMotorPin1 = 6;
 const int rightMotorPin2 = 5;
@@ -39,6 +43,7 @@ Servo gripper;
 int gripperPin = 12;
 int pinServo = 12;
 
+//Set the int needed
 int s0;
 int s1;
 int s2;
@@ -47,6 +52,7 @@ int s4;
 int s5;
 int s6;
 int s7;
+int endLineCounter = 0;
 
 const byte interruptPin1 = 2;
 const byte interruptPin2 = 3;
@@ -62,17 +68,18 @@ const int motorSpeed = 255;
 // Define the threshold for detecting the black line
 const int threshold = 800;
 
-
+//some functions declaration
 bool startLineFunctionExecuted = false;
 bool endLineFunctionExecuted = false;
 
+//================ START OF SETUP  ====================
 void setup() {
   // Initialize the serial communication
   Serial.begin(9600);
 
   // Initialize the Neopixel strip
   pixels.begin();
-  
+
   // Set distance sensor pins
   pinMode(Trig, OUTPUT);
   pinMode(Echo, INPUT);
@@ -90,10 +97,13 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(interruptPin2), counterRight, CHANGE);
 }
 
+//================ END OF SETUP  ===============================================
+
+//================ START OF LOOP  ===============================================
 void loop() {
 
-//========start of loop for neopixels=========================================
-    // Set the color of the first pixel to red
+  //========start of loop for neopixels=========================================
+  // Set the color of the first pixel to red
   pixels.setPixelColor(0, pixels.Color(0, 255, 0)); // Red
 
   // Set the color of the second pixel to green
@@ -108,18 +118,18 @@ void loop() {
   // Show the updated colors on the Neopixel strip
   pixels.show();
 
+  //======== end of loop for neopixels=========================================
+  
+  if (!startLineFunctionExecuted) {
 
-//======== end of loop for neopixels=========================================
-   if(!startLineFunctionExecuted){
-   
-     startLine();
+    startLine();
 
     startLineFunctionExecuted = true;
   }
 
- if(endLineCounter > 1){
-  halt(); 
- }
+  if (endLineCounter > 1) {
+    halt();
+  }
 
   // Measure distance to nearby objects us bing the ultrasonic sensor
   long duration, distance;
@@ -131,16 +141,16 @@ void loop() {
   duration = pulseIn(Echo, HIGH);
   distance = duration * 0.034 / 2;
 
-// Print distance sensor readings
+  // Print distance sensor readings
   Serial.print(distance);
   Serial.print("cm");
   Serial.println();
 
-  if(distance < 13 ){
+  if (distance < 13 ) {
     avoidObstacle();
     findLine();
-    }
-  
+  }
+
   // Read the sensor values
   int sensor1 = analogRead(sensorPin1);
   int sensor2 = analogRead(sensorPin2);
@@ -169,47 +179,45 @@ void loop() {
   Serial.print(sensor8);
   Serial.println();
 
-  if(sensor1 > 900 && sensor2 > 900 && sensor3 > 900 && sensor4 > 900 && sensor5 > 900 && sensor6 > 900 && sensor7 > 900 && sensor8 > 900){
-   goforward();
-   delay(100);
-    // Read the sensor values again
-  int sensor1 = analogRead(sensorPin1);
-  int sensor2 = analogRead(sensorPin2);
-  int sensor3 = analogRead(sensorPin3);
-  int sensor4 = analogRead(sensorPin4);
-  int sensor5 = analogRead(sensorPin5);
-  int sensor6 = analogRead(sensorPin6);
-  int sensor7 = analogRead(sensorPin7);
-  int sensor8 = analogRead(sensorPin8);
-
-  if(sensor1 > 950 && sensor2 > 950 && sensor3 > 950 && sensor4 > 950 && sensor5 > 950 && sensor6 > 950 && sensor7 > 950 && sensor8 > 950){
-   halt();
-  
-  openGripper();
-  delay(500); // Wait for the gripper to open
-   moveBack();
-  delay(1000);
-  endLineCounter++;
-  }
-  
-  
-  }
-  
-    else if(sensor1 > threshold || sensor2 > threshold || sensor3 > threshold){
-      turnLeft();
-      }
-      else if(sensor3 > threshold){
-        turnLeft();
-        }
-        else if(sensor7 > threshold || sensor8 > threshold){
-          turnRight();
-          }
-          else if(sensor6 > threshold){
-            turnRight();
-            }
-    else if(sensor4 > threshold || sensor5 > threshold ){
+  if (sensor1 > 900 && sensor2 > 900 && sensor3 > 900 && sensor4 > 900 && sensor5 > 900 && sensor6 > 900 && sensor7 > 900 && sensor8 > 900) {
     goforward();
+    delay(100);
+    // Read the sensor values again
+    int sensor1 = analogRead(sensorPin1);
+    int sensor2 = analogRead(sensorPin2);
+    int sensor3 = analogRead(sensorPin3);
+    int sensor4 = analogRead(sensorPin4);
+    int sensor5 = analogRead(sensorPin5);
+    int sensor6 = analogRead(sensorPin6);
+    int sensor7 = analogRead(sensorPin7);
+    int sensor8 = analogRead(sensorPin8);
+
+    if (sensor1 > 950 && sensor2 > 950 && sensor3 > 950 && sensor4 > 950 && sensor5 > 950 && sensor6 > 950 && sensor7 > 950 && sensor8 > 950) {
+      halt();
+
+      openGripper();
+      delay(500); // Wait for the gripper to open
+      moveBack();
+      delay(1000);
+      endLineCounter++;
     }
+  }
+
+  else if (sensor1 > threshold || sensor2 > threshold || sensor3 > threshold) {
+    turnLeft();
+  }
+  else if (sensor3 > threshold) {
+    turnLeft();
+  }
+  else if (sensor7 > threshold || sensor8 > threshold) {
+    turnRight();
+  }
+  else if (sensor6 > threshold) {
+    turnRight();
+  }
+  else if (sensor4 > threshold || sensor5 > threshold ) {
+    goforward();
+  }
 }
 
 
@@ -229,49 +237,49 @@ void avoidObstacle() {
   startTime = millis();
   while (millis() - startTime < driveDuration) {
     goforward();
-  } 
+  }
   // Drive forward for a short time
-  
+
   startTime = millis();
   while (millis() - startTime < turn2Duration) {
     turnRightObstacle();
   }
-
 }
 
-void findLine(){
+void findLine() {
 
   int linedetector = 0;
-  
-  while(linedetector == 0){
 
-  int sensor1 = analogRead(sensorPin1);
-  int sensor2 = analogRead(sensorPin2);
-  int sensor3 = analogRead(sensorPin3);
-  int sensor4 = analogRead(sensorPin4);
-  int sensor5 = analogRead(sensorPin5);
-  int sensor6 = analogRead(sensorPin6);
-  int sensor7 = analogRead(sensorPin7);
-  int sensor8 = analogRead(sensorPin8);
-  
-  goforward();
-  
-  if(sensor1 > 700 || sensor2 > 700 || sensor3 > 700 || sensor4 > 700 || sensor5 > 700 || sensor6 > 700 || sensor7 > 700 || sensor8 > 700){
-    linedetector = 1;
+  while (linedetector == 0) {
+
+    int sensor1 = analogRead(sensorPin1);
+    int sensor2 = analogRead(sensorPin2);
+    int sensor3 = analogRead(sensorPin3);
+    int sensor4 = analogRead(sensorPin4);
+    int sensor5 = analogRead(sensorPin5);
+    int sensor6 = analogRead(sensorPin6);
+    int sensor7 = analogRead(sensorPin7);
+    int sensor8 = analogRead(sensorPin8);
+
+    goforward();
+
+    if (sensor1 > 700 || sensor2 > 700 || sensor3 > 700 || sensor4 > 700 || sensor5 > 700 || sensor6 > 700 || sensor7 > 700 || sensor8 > 700) {
+      linedetector = 1;
     }
   }
-  
 }
 
+//================ END OF LOOP  ===============================================================
 
+//================ START FUNCTION DECLARATIONS  ===============================================
 // Function definition for moving forward
 void goforward() {
-  if(counter1 <= inter && counter2<= inter) {
-  // Move both motors forward
-  analogWrite(leftMotorPin1, 255);
-  analogWrite(leftMotorPin2, 0);
-  analogWrite(rightMotorPin1, 255);
-  analogWrite(rightMotorPin2, 0);
+  if (counter1 <= inter && counter2 <= inter) {
+    // Move both motors forward
+    analogWrite(leftMotorPin1, 255);
+    analogWrite(leftMotorPin2, 0);
+    analogWrite(rightMotorPin1, 255);
+    analogWrite(rightMotorPin2, 0);
   } else {
     halt();
     counterReset();
@@ -280,84 +288,81 @@ void goforward() {
 
 // Define function to turn left
 void turnLeft() {
-  if(counter1 <= inter && counter2<= inter) {
-  analogWrite(leftMotorPin1, 0);
-  analogWrite(leftMotorPin2, 0);
-  analogWrite(rightMotorPin1, 200);
-  analogWrite(rightMotorPin2, 0);
-  }else {
+  if (counter1 <= inter && counter2 <= inter) {
+    analogWrite(leftMotorPin1, 0);
+    analogWrite(leftMotorPin2, 0);
+    analogWrite(rightMotorPin1, 200);
+    analogWrite(rightMotorPin2, 0);
+  } else {
     halt();
     counterReset();
   }
 }
 
+// Define function to turn left to avoid obstacle
 void turnLeftObstacle() {
-  if(counter1 <= inter && counter2<= inter) {
-  analogWrite(leftMotorPin1, 0);
-  analogWrite(leftMotorPin2, 255);
-  analogWrite(rightMotorPin1, 255);
-  analogWrite(rightMotorPin2, 0);
-  }else {
+  if (counter1 <= inter && counter2 <= inter) {
+    analogWrite(leftMotorPin1, 0);
+    analogWrite(leftMotorPin2, 255);
+    analogWrite(rightMotorPin1, 255);
+    analogWrite(rightMotorPin2, 0);
+  } else {
     halt();
     counterReset();
   }
 }
 
+// Define function to turn right to avoid obstacle
 void turnRightObstacle() {
-  if(counter1 <= inter && counter2<= inter) {
-  analogWrite(leftMotorPin1, 255);
-  analogWrite(leftMotorPin2, 0);
-  analogWrite(rightMotorPin1, 0);
-  analogWrite(rightMotorPin2, 255);
-  }else {
+  if (counter1 <= inter && counter2 <= inter) {
+    analogWrite(leftMotorPin1, 255);
+    analogWrite(leftMotorPin2, 0);
+    analogWrite(rightMotorPin1, 0);
+    analogWrite(rightMotorPin2, 255);
+  } else {
     halt();
     counterReset();
   }
 }
-
+// Define function to turn slow left
 void slowLeft() {
-  if(counter1 <= inter && counter2<= inter) {
-  analogWrite(leftMotorPin1, 155);
-  analogWrite(leftMotorPin2, 0);
-  analogWrite(rightMotorPin1, 255);
-  analogWrite(rightMotorPin2, 0);
-  }else {
+  if (counter1 <= inter && counter2 <= inter) {
+    analogWrite(leftMotorPin1, 155);
+    analogWrite(leftMotorPin2, 0);
+    analogWrite(rightMotorPin1, 255);
+    analogWrite(rightMotorPin2, 0);
+  } else {
     halt();
     counterReset();
   }
 }
 
+// Define function to turn slow right
 void slowRight() {
-  if(counter1 <= inter && counter2<= inter) {
-  analogWrite(leftMotorPin1, 255);
-  analogWrite(leftMotorPin2, 0);
-  analogWrite(rightMotorPin1, 155);
-  analogWrite(rightMotorPin2, 0);
-  }else {
+  if (counter1 <= inter && counter2 <= inter) {
+    analogWrite(leftMotorPin1, 255);
+    analogWrite(leftMotorPin2, 0);
+    analogWrite(rightMotorPin1, 155);
+    analogWrite(rightMotorPin2, 0);
+  } else {
     halt();
     counterReset();
   }
-
-
 }
 
 // Define function to turn right
 void turnRight() {
-  if(counter1 <= inter && counter2<= inter) {
-  analogWrite(leftMotorPin1, 200
-  );
-  analogWrite(leftMotorPin2, 0);
-  analogWrite(rightMotorPin1, 0);
-  analogWrite(rightMotorPin2, 0);
-}else {
+  if (counter1 <= inter && counter2 <= inter) {
+    analogWrite(leftMotorPin1, 200
+               );
+    analogWrite(leftMotorPin2, 0);
+    analogWrite(rightMotorPin1, 0);
+    analogWrite(rightMotorPin2, 0);
+  } else {
     halt();
     counterReset();
   }
-
-
-  
 }
-
 
 // Define function to halt
 void halt() {
@@ -367,64 +372,65 @@ void halt() {
   analogWrite(rightMotorPin2, 0);
 }
 
+// Define function to counterLeft
 void counterLeft() {
   noInterrupts();
   counter1++;
   interrupts();
 }
 
+// Define function to counterRight
 void counterRight() {
   noInterrupts();
   counter2++;
   interrupts();
 }
 
+// Define function to counterReset
 void counterReset() {
   counter1 = 0;
   counter2 = 0;
 }
 
-
+// Define function to move from parking spot
 void startLine() {
- moveForward();
+  moveForward();
   delay(500);
 
   closeGripper();
   Leftturn ();
-   moveForward();
+  moveForward();
   delay(50);
 }
 
-
 // Function definition for moving forward
-  
- void moveForward() {
 
+void moveForward() {
   analogWrite(leftMotorPin1, 255);
   analogWrite(leftMotorPin2, 0);
   analogWrite(rightMotorPin1, 255);
   analogWrite(rightMotorPin2, 0);
-  }
+}
 
-   void moveBack() {
-
+// Function definition for moving backward
+void moveBack() {
   analogWrite(leftMotorPin1, 0);
   analogWrite(leftMotorPin2, 255);
   analogWrite(rightMotorPin1, 0);
   analogWrite(rightMotorPin2, 255);
-  
-  }
+}
 
-    // Turn left
-  void Leftturn() {
+//Function definition to Turn left
+void Leftturn() {
   analogWrite(leftMotorPin1, 150);
   analogWrite(leftMotorPin2, 0);
   analogWrite(rightMotorPin1, 0);
   analogWrite(rightMotorPin2, 0);
   delay(1000);
-  }
+}
 
- // forloop 8-10x
+//Function definition to close Gripper
+// forloop 8-10x
 void closeGripper()
 {
   for (int i = 0; i < 10; i++)
@@ -436,6 +442,7 @@ void closeGripper()
   }
 }
 
+//Function definition to open Gripper
 void openGripper()
 {
   for (int i = 0; i < 10; i++)
@@ -447,8 +454,9 @@ void openGripper()
   }
 }
 
+//Function definition for dropping the cone and moving backwards
 void endLine() {
- openGripper();
+  openGripper();
   delay(500);
   moveBack();
   delay(500);
@@ -458,10 +466,4 @@ void endLine() {
   endLineCounter++;
 }
 
-
- // if(!endLineFunctionExecuted){
-   
-  //   endLine();
-
- //   endLineFunctionExecuted = true;
- // }
+//================ END FUNCTION DECLARATIONS  ===============================================
